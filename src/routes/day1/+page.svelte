fed0d476827c8f934f87c43e5009d7b1ad572a06
mumbler9486 <mumbler9486@gmail.com>
2024-12-02 23:11:36 +0900

    ✔ Finish day 1

--- src/routes/day1/+page.svelte
+++ src/routes/day1/+page.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+	import FlakyComponent from './FlakyComponent.svelte';
+
+	let error = $state<unknown>(null);
+	let reset = $state(() => {});
+
+	function onerror(e: unknown, r: () => void) {
+		error = e;
+		reset = r;
+	}
 </script>
 
-<div>Hello world</div>
+<svelte:boundary {onerror}><FlakyComponent /></svelte:boundary>
+
+{#if error}
+	<button
+		class="btn btn-error"
+		onclick={() => {
+			error = null;
+			reset();
+		}}
+	>
+		Oops, Try again!
+	</button>
+{/if}
---
+++ src/routes/day1/FlakyComponent.svelte
@@ -0,0 +1,29 @@
+<script>
+	let mouse = $state({ x: 0, y: 0 });
+	let success = $state(false);
+</script>
+
+<svelte:window
+	onmousemove={(e) => {
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+	}}
+/>
+
+<p class="hidden">{mouse.x}x{mouse.y}</p>
+
+<button
+	class="btn btn-primary"
+	class:hidden={success}
+	onclick={() => {
+		if (Math.random() > 0.2) {
+			mouse = null;
+		} else {
+			success = true;
+		}
+	}}>Be naughty?</button
+>
+
+{#if success}
+	<h1 class="text-5xl font-bold text-green-500">Success!</h1>
+{/if}
