44a82adad9677f8ce2bec051523fd55e67b1e395
mumbler9486 <mumbler9486@gmail.com>
2024-12-05 22:31:33 +0900

    ✔ Finish day 3

---
+++ src/routes/day3/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { rollPresent } from './Snippet.svelte';
+
+	const getRandomInt = () => Math.floor(Math.random() * 100);
+	const numPresents = Array.from({ length: 10 }, (_, i) => i + 1);
+</script>
+
+<div class="flex flex-wrap gap-4">
+	{#each numPresents}
+		{@render rollPresent(getRandomInt())}
+	{/each}
+</div>
---
+++ src/routes/day3/Snippet.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" module>
+	export { rollPresent };
+</script>
+
+<span>Me outside the snippet</span>
+
+{#snippet rollPresent(luckyNum: number)}
+	{#if luckyNum % 2 === 0}
+		<div class="w-20">
+			<img src="/star.png" alt="Star" />
+			<p>Got a Star!</p>
+		</div>
+	{/if}
+
+	{#if luckyNum % 2 === 1}
+		<div class="w-24">
+			<img class="bg-red-900" src="/coal.png" alt="Coal" />
+			<span>Got coal 😞</span>
+		</div>
+	{/if}
+{/snippet}
