3840111e2a67a2d19b4b266ce919cc2556612f7f
mumbler9486 <mumbler9486@gmail.com>
2024-12-12 22:59:22 +0900

    ✔ Finish day 7

---
+++ src/routes/day7/+page.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+</script>
+
+<h1 class="mb-8 text-5xl">🎅 North Pole Diner 🍔</h1>
+
+<details open={true}>
+	<summary class="text-xl">Appetizers</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			Basket Oho Fries <span class="float-right">13.99</span>
+		</li>
+		<li class="w-full">
+			Taco Fritters <span class="float-right">16.99</span>
+		</li>
+		<li class="w-full">
+			Buttered Nachos <span class="float-right">7.50</span>
+		</li>
+		<li class="w-full">
+			Reindeer Tater Tots <span class="float-right">6.50</span>
+		</li>
+	</ul>
+</details>
+<details open={true}>
+	<summary class="cursor-pointer text-xl">Burgers</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			The Classic <span class="float-right">13.99</span>
+		</li>
+		<li class="w-full">
+			Northern Special <span class="float-right">16.99</span>
+		</li>
+		<li class="w-full">
+			Santa's Favorite <span class="float-right">15.98</span>
+		</li>
+		<li class="w-full">
+			Elven Kids Set <span class="float-right">8.99</span>
+		</li>
+	</ul>
+</details>
+<details open={true}>
+	<summary class="text-xl">Salads</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			Elvish Menagerie <span class="float-right">14.99</span>
+		</li>
+		<li class="w-full">
+			Cornucopian Corn Special <span class="float-right">7.99</span>
+		</li>
+		<li class="w-full">
+			Ginger Crumb Salad <span class="float-right">11.50</span>
+		</li>
+	</ul>
+</details>
+<details open={true}>
+	<summary class="text-xl">Soups</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			Creamy Corn Egg <span class="float-right">5.99</span>
+		</li>
+		<li class="w-full">
+			Homemade Chicken Noodle <span class="float-right">8.99</span>
+		</li>
+		<li class="w-full">
+			Lucky Cheesy Potato<span class="float-right">11.50</span>
+		</li>
+	</ul>
+</details>
+<details open={true}>
+	<summary class="text-xl">Desert</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			Elvish Delights Fruit Cake <span class="float-right">17.99</span>
+		</li>
+		<li class="w-full">
+			Candy Cane Ice Creme <span class="float-right">4.99</span>
+		</li>
+		<li class="w-full">
+			Santa's Portable Ice Creme Cookie Sandwich <span class="float-right">4.99</span>
+		</li>
+		<li class="w-full">
+			Fairy Dust Sundae <span class="float-right">8.20</span>
+		</li>
+		<li class="w-full">
+			Ginger Bread Cake (slice)<span class="float-right">6.20</span>
+		</li>
+	</ul>
+</details>
+<details open={true}>
+	<summary class="text-xl">Drinks</summary>
+	<ul class="ml-8 flex max-w-xl flex-col content-around">
+		<li class="w-full">
+			Original Craft Cola <span class="float-right">3.99</span>
+		</li>
+		<li class="w-full">
+			Original Craft Cola Float <span class="float-right">4.50</span>
+		</li>
+		<li class="w-full">
+			Strawberry Lemon Smoothie <span class="float-right">8.00</span>
+		</li>
+		<li class="w-full">
+			Spicy Ginger Ale <span class="float-right">2.99</span>
+		</li>
+		<li class="w-full">
+			Lemonade <span class="float-right">2.99</span>
+		</li>
+		<li class="w-full">
+			Water <span class="float-right">1.99</span>
+		</li>
+	</ul>
+</details>
