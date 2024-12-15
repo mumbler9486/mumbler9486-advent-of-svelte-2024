5bce11cb543b725c85537d04296ef375f8752387
mumbler9486 <mumbler9486@gmail.com>
2024-12-15 11:43:25 +0900

    ✔ Finish day 9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
 					<a class="btn btn-primary" href="/day6">Day 6</a>
 					<a class="btn btn-primary" href="/day7">Day 7</a>
 					<a class="btn btn-primary" href="/day8">Day 8</a>
-					<a class="pointer-events-none" href="/day9">Day 9</a>
+					<a class="btn btn-primary" href="/day9">Day 9</a>
 					<a class="pointer-events-none" href="/day10">Day 10</a>
 					<a class="pointer-events-none" href="/day11">Day 11</a>
 					<a class="pointer-events-none" href="/day12">Day 12</a>
---
+++ src/routes/day9/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+
+	const textSize = 24;
+	const textOffset = { x: 10, y: 25 };
+
+	let presentDestination = $state({
+		destination: 'Boston, MA'
+	});
+</script>
+
+<h1 class="mb-8 text-5xl">Cheapestly Coded Present Labeller</h1>
+<p>
+	See console for warnings/errors and notifications for sent with links to documentation if issue.
+</p>
+<Input label="To" placeholder="Type here" bind:value={presentDestination.destination} />
+<button
+	class="btn btn-primary mt-4"
+	onclick={() => {
+		console.log(presentDestination);
+	}}>Generate Present</button
+>
+<svg class="mt-4 h-96 w-full bg-neutral-content" role="presentation">
+	<text font-size={textSize} x={textOffset.x} y={textOffset.y}
+		>To: {presentDestination.destination}</text
+	>
+	<text font-size={textSize} x={textOffset.x} y={textOffset.y + textSize}>From: The North Pole</text
+	>
+	<text font-size={textSize} x={textOffset.x} y={textOffset.y + textSize * 3}
+		>Deliver By December 25, 2024 🎁</text
+	>
+</svg>
