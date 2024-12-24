848974286553b20bf2cce6385a26b8c4cfe85766
mumbler9486 <mumbler9486@gmail.com>
2024-12-25 02:58:09 +0900

    🔊 Add info to logs for day 10

--- src/lib/stores/magicNumber.svelte.ts
+++ src/lib/stores/magicNumber.svelte.ts
@@ -5,8 +5,8 @@ let magicNumberState = $state(false);
 export const getMagicNumberState = () => magicNumberState;
 
 export const prepareMagicNumber = async () => {
-	console.log('Preparing Magic Number Creator...');
+	console.log('[Day 10] Preparing Magic Number Creator...');
 	await new Promise((resolve) => setTimeout(resolve, 2000));
 	magicNumberState = true;
-	console.log('Finished creating Magic Number Creator...');
+	console.log('[Day 10] Finished creating Magic Number Creator...');
 };
