7ad27970822c8c80970e4e8685619f67c8c064bb
mumbler9486 <mumbler9486@gmail.com>
2024-12-26 04:55:07 +0900

    💬 Update day 21 text

--- src/routes/day20/+page.svelte
+++ src/routes/day20/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 </script>
 
-<h1 class="mb-8 text-5xl">Svelte Playground Demo</h1>
+<h1 class="mb-8 text-5xl">Svelte Playground Demo in VIM mode</h1>
 <p>
 	Checking out the VIM mode in Svelte Playground. See <a href="/day18">Day 18</a> for the original demo.
 </p>
 <a
 	class="btn btn-primary mt-4"
 	href="https://svelte.dev/playground/2a3b3f35d6fe4a4ca2cf457b5e6ddcb3?version=5.15.0"
-	>Demo (Svelte Playground in VIM mode)</a
+	>Day 18 Demo (Svelte Playground in VIM mode)</a
 >
 
 <img src="/day19-vim-mode.png" alt="How to turn on VIM mode in the playground" />
